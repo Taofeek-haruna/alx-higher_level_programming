8f9ecfbef7741b3e8b4ab932dd30a17d8d4021b3
Taofeek-haruna <taofeekharuna5@gmail.com>
2023-01-05 19:15:01 -0800

    second

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def argu_no(*args):
+    return(len(args))
+
+    c = 6
+    f = 3
+
+    n = argu_no(Hello, welcome, to, the, best, school)
+
+
+print(n, "arguments:\n, 1:Hello\n, 2:welcome\n")
+print("3:to\n, 4:the\n, 5:best\n, 6:school\n")
