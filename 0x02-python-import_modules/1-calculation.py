fe37c968fafab95dfff5cc2673fb577644486a0c
Taofeek-haruna <taofeekharuna5@gmail.com>
2023-01-05 17:29:36 -0800

    second

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+def sub(a, b):
+
+    a = 10
+    b = 5
+    result = a - b
+
+
+print(f'{a} - {b} = {result}\n')
+
+
+def add(a, b):
+
+    a = 10
+    b = 5
+    result = a + b
+
+
+print(f'{a} + {b} = {result}\n')
+
+
+def mul(a, b):
+
+    a = 10
+    b = 5
+    result = a * b
+
+
+print(f'{a} * {b} = {result}\n')
+
+
+def div(a, b):
+
+    a = 10
+    b = 5
+    result = a / b
+
+
+print(f'{a} / {b} = {result}\n')
---
+++ 0x02-python-import_modules/calculator_1.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def add(a, b):
+
+a = 10
+b = 5
+
+return value. a + b
+return (a + b)
